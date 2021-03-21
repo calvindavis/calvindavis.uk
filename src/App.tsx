34906274818d11c86662ab5d2fd0ca99e2fe743d
Calvin Davis <hi@calvindavis.co.uk>
2021-03-21 20:18:55 +0000

    feat: style home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function App(): JSX.Element {
-	return <div className="container mx-auto font-bold">We're rolling! 🎬</div>;
+	return (
+		<div className="w-screen h-screen flex items-center justify-center p-4 bg-black">
+			<h1 className="max-w-xl lg:max-w-4xl text-5xl lg:text-7xl font-bold text-grey">
+				<>My name is </>
+				<span className="gradient-text bg-gradient-to-r from-orange to-yellow">
+					Calvin Davis
+				</span>
+				. I'm a developer, average guitarist, and all-round great guy.
+			</h1>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
 module.exports = {
 	purge: ["./src/**/*.html", "./src/**/*.tsx"],
+	theme: {
+		colors: {
+			black: "#07080a",
+			blue: "#084f93",
+			grey: "#878581",
+			orange: "#d88144",
+			yellow: "#ffc357",
+		},
+	},
 };
